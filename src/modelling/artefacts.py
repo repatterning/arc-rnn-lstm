2f55dcabbda240694c449f4850c787e83edde486
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-10-28 19:55:38 +0000

    Update src/modelling/artefacts.py

--- src/modelling/artefacts.py
+++ src/modelling/artefacts.py
@@ -1,11 +1,13 @@
 """Module artefacts.py"""
 import logging
+import os
 import pandas as pd
 import tensorflow as tf
 
 import src.elements.intermediary as itr
 import src.elements.master as mr
 import src.elements.sequences as sq
+import src.functions.streams
 
 
 class Artefacts:
@@ -21,6 +23,21 @@ class Artefacts:
 
         self.__arguments = arguments
 
+        # Instances
+        self.__streams = src.functions.streams.Streams()
+
+    def __history(self, model: tf.keras.src.models.Sequential, path: str):
+        """
+
+        :param model:
+        :param path:
+        :return:
+        """
+
+        history = pd.DataFrame(data=model.history.history)
+
+        return self.__streams.write(blob=history, path=os.path.join(path, 'history.csv'))
+
     # noinspection PyUnresolvedReferences
     def exc(self, model: tf.keras.src.models.Sequential, sequences: sq.Sequences, intermediary: itr.Intermediary, master: mr.Master) -> str:
         """
@@ -32,14 +49,11 @@ class Artefacts:
         :return:
         """
 
-        history = pd.DataFrame(data=model.history.history)
-        history.info()
+        self.__history(model=model, path=master.path)
 
-        # Arguments
-        logging.info(self.__arguments)
 
-        # The training sets
-        intermediary.training.info()
-        master.training.info()
+
+
+
 
         return 'in progress'
