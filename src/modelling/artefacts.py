022f9060a111429f86ba4032b6dfc2c5a5119887
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-10-28 17:15:28 +0000

    Update src/modelling/artefacts.py: In Progress

--- src/modelling/artefacts.py
+++ src/modelling/artefacts.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 
 import src.elements.intermediary as itr
 import src.elements.master as mr
+import src.elements.sequences as sq
 
 
 class Artefacts:
@@ -21,10 +22,11 @@ class Artefacts:
         self.__arguments = arguments
 
     # noinspection PyUnresolvedReferences
-    def exc(self, model: tf.keras.src.models.Sequential, intermediary: itr.Intermediary, master: mr.Master) -> str:
+    def exc(self, model: tf.keras.src.models.Sequential, sequences: sq.Sequences, intermediary: itr.Intermediary, master: mr.Master) -> str:
         """
 
         :param model:
+        :param sequences:
         :param intermediary:
         :param master:
         :return:
