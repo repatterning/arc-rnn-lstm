8b083b546fbd1a478cf6690211bf851fc6d9d54a
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-10-28 19:57:30 +0000

    Update src/modelling/estimates.py: In Progress

---
+++ src/modelling/estimates.py
@@ -0,0 +1,13 @@
+
+class Estimates:
+    """
+    Estimates
+    """
+
+    def __init__(self, arguments: dict):
+        """
+
+        :param arguments: A set of arguments vis-à-vis calculation & storage objectives.
+        """
+
+        self.__arguments = arguments
