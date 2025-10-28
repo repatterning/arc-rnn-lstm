cf6739b4fa8462949f4132ef11144d33cb586f52
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-10-28 16:26:47 +0000

    Update src/modelling/persist.py: Re-purposing

--- src/modelling/persist.py
+++ src/modelling/persist.py
@@ -5,7 +5,6 @@ import pandas as pd
 
 import config
 import src.elements.partitions as pr
-import src.elements.inference as ifr
 import src.functions.streams
 
 
@@ -32,23 +31,13 @@ class Persist:
 
         return self.__streams.write(blob=blob, path=path)
 
-    def exc(self, inference: ifr.Inference, partition: pr.Partitions) -> str:
+    def exc(self, partition: pr.Partitions):
         """
 
-        :param inference:
         :param partition:
         :return:
         """
 
 
-        endpoint = os.path.join(
+        os.path.join(
             self.__configurations.assets_, str(partition.catchment_id), str(partition.ts_id))
-
-        message = '|'.join((
-            str(partition.catchment_id),
-            str(partition.ts_id),
-            self.__persist(blob=inference.evidence_lower_bound, path=os.path.join(endpoint, 'evidence_lower_bound.csv')),
-            self.__persist(blob=inference.estimates, path=os.path.join(endpoint, 'estimates.csv'))
-        ))
-
-        return message
