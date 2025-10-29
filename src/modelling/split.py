cd66606b1024612a4f39a61e40334e191f868861
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-10-29 20:51:21 +0000

    Update src/modelling/split.py

--- src/modelling/split.py
+++ src/modelling/split.py
@@ -80,6 +80,6 @@ class Split:
 
         # Persist
         for instances, name in zip([frame, training, testing], ['data.csv', 'training.csv', 'testing.csv']):
-            self.__persist(blob=instances.drop(columns='date'), pathstr=os.path.join(path, name))
+            self.__persist(blob=instances, pathstr=os.path.join(path, name))
 
         return mr.Master(training=training, testing=testing, path=path)
